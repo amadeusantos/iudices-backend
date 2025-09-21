9f1a1834d2f1727c835ab4c1aab45991db0f1bd5
amadeusantos <amadeusantos31@gmail.com>
2025-09-21 00:14:47 -0300

    feat: integration entity user and oauth2

--- api/schemas.py
+++ api/schemas.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from uuid import UUID
+
 from pydantic import BaseModel, ConfigDict
 
 
@@ -7,3 +10,11 @@ class InputSchema(BaseModel):
 
 class BaseSchema(BaseModel):
     model_config = ConfigDict(use_enum_values=True, str_strip_whitespace=True)
+
+
+class UserPrincipal(BaseSchema):
+    id: Optional[UUID] = None
+    name: Optional[str] = None
+    email: Optional[str] = None
+    google_openid: Optional[str] = None
+
